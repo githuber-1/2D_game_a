2fc44f236da5589eeb4720000c3dbadb527eddb1
Ian Huber <36930036+IanHuber@users.noreply.github.com>
2020-10-06 23:12:56 -0600

    init repo

---
+++ Assets/Bro/Bro.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bro : MonoBehaviour
+{
+    public float speed = 15.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+       
+        if (Input.GetKeyDown("right"))
+        {
+            if(transform.rotation != Quaternion.Euler(0, 0, 0))
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            if (transform.rotation != Quaternion.Euler(0, 180, 0))
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+        }
+
+        float translation_y = Input.GetAxis("Vertical") * speed;
+        float translation_x = Input.GetAxis("Horizontal") * speed;
+
+        translation_y *= Time.deltaTime;
+        translation_x *= Time.deltaTime;
+
+        transform.Translate(0, translation_y, 0, Space.World);
+        transform.Translate(translation_x, 0, 0, Space.World);
+
+    }
+
+    private void Flip()
+    {
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
---
+++ Assets/Missile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision);
+        Enemy enemy = collision.GetComponent<Enemy>();
+        if(enemy != null)
+        {
+            enemy.TakeDamage(30);
+        }
+        Destroy(gameObject);
+    }
+}
